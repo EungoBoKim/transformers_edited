58dcb0c45751809e53c14120428f06dc7e74330f
jinyingfu <rladmdqh0214@naver.com>
2022-01-14 22:53:30 +0900

    upload files

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="transformers_edited",
+    version="0.0.1",
+    author="ebk",
+    author_email="rladmdqh0214@naver.com",
+    description="A small example package",
+    long_description="aA long example package",
+    long_description_content_type="text/markdown",
+    url="https://github.com/EungoBoKim/transformers_edited.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
